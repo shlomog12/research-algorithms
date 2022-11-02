9d6b872d61ea94660e69ca00fe4e361e6c157b56
shlomog12 <73135976+shlomog12@users.noreply.github.com>
2022-11-02 05:10:31 +0200

    finish hw1

--- home_works/hw1/q2.py
+++ home_works/hw1/q2.py
@@ -1,7 +1,31 @@
+"""
+ Author: Shlomo Glick
+ Since : 2022-10
+"""
+
 from typing import Callable, Any
 
-
 def extract_path_from_dict_of_parents(parnts: dict, start, end):
+    """
+    The function receives a start node, an end node, a dictionary
+    that indicates for each node where we got to.
+    The function returns an ordered list of the track from beginning to end
+
+    test1
+    >>> extract_path_from_dict_of_parents(parnts={1:3, 3:5, 5:7, 6:1}, start=7, end=6)
+    [7, 5, 3, 1, 6]
+
+    test2
+    >>> extract_path_from_dict_of_parents(parnts={1:2, 2:4, 5:1, 32:7, 4:32, 10:12}, start=7, end=5)
+    [7, 32, 4, 2, 1, 5]
+
+    test3
+    >>> extract_path_from_dict_of_parents(parnts={1:2, 2:4, 5:1, 32:7, 4:32, 10:12}, start=7, end=12)
+    not exists path
+    """
+    
+    if end not in parnts:
+        return "not exists path"
     path = []
     current = end
     while current != start:
@@ -12,6 +36,37 @@ def extract_path_from_dict_of_parents(parnts: dict, start, end):
 
 
 def breadth_first_search(start: Any, end: Any, neighbor_function: Callable):
+    """
+    This is a function that receives a start node,
+    an end node and a function that returns the node's neighbors
+    and returns a track from the beginning to the end
+
+    TESETS
+
+    test1
+    >>> breadth_first_search(start=(1, 3), end=(2, 7), neighbor_function=four_neighbor_function1)
+    ['(1, 3)', '(2, 3)', '(2, 4)', '(2, 5)', '(2, 6)', '(2, 7)']
+
+    test2
+    >>> breadth_first_search(start={"x": 1, "y": 7}, end={"x": 2, "y": 9}, neighbor_function=four_neighbor_function2)
+    ["{'x': 1, 'y': 7}", "{'x': 2, 'y': 7}", "{'x': 2, 'y': 8}", "{'x': 2, 'y': 9}"]
+
+    test3
+    >>> G = Graph()
+    >>> G.addNode(1)
+    >>> G.addNode(2)
+    >>> G.addNode(3)
+    >>> G.addNode(4)
+    >>> G.addNode(5)
+    >>> G.addconnection(1,4)
+    >>> G.addconnection(4,5)
+    >>> G.addconnection(5,2)
+    >>> G.addconnection(2,3)
+    >>> G.addconnection(5,3)
+    >>> breadth_first_search(start=1, end=3, neighbor_function=G.getNeibyId)
+    ['1', '4', '5', '3']
+    """
+
     if start == end:
         return start
     parnts = {}
@@ -32,26 +87,20 @@ def breadth_first_search(start: Any, end: Any, neighbor_function: Callable):
 
 
 
-#  ################################  TESTS ########################################
+#  ################################  FORֹ TESTS ########################################
 
 
-# def four_neighbor_function(node:Any)->list:
-#     (x,y) = node
-#     return [(x+1,y), (x-1,y), (x,y+1), (x,y-1)]
+def four_neighbor_function1(node:Any)->list:
+    (x,y) = node
+    return [(x+1,y), (x-1,y), (x,y+1), (x,y-1)]
 
-# def four_neighbor_function(node:Any)->list:
-
-#     return [
-#         {"x": node["x"]+1, "y": node["y"]},
-#         {"x": node["x"]-1, "y": node["y"]},
-#         {"x": node["x"], "y": node["y"]+1},
-#         {"x": node["x"], "y": node["y"]-1},
-#         ]
-
-# start = {"x": 1, "y": 7}
-# end = {"x": 2, "y": 9}
-# start = (1,7)
-# end = (2,9)
+def four_neighbor_function2(node:Any)->list:
+    return [
+        {"x": node["x"]+1, "y": node["y"]},
+        {"x": node["x"]-1, "y": node["y"]},
+        {"x": node["x"], "y": node["y"]+1},
+        {"x": node["x"], "y": node["y"]-1},
+        ]
 
 
 class Graph:
@@ -69,9 +118,6 @@ class Graph:
         self.nodes[id1].addNei(id2)
         self.nodes[id2].addNei(id1)
 
-
-
-
 class Node:
     def __init__(self, id):
         self.id = id
@@ -85,27 +131,9 @@ class Node:
         return self.nei
 
 
+def main():
+    import doctest
+    print(doctest.testmod())
 
-
-
-G = Graph()
-G.addNode(1)
-G.addNode(2)
-G.addNode(3)
-G.addNode(4)
-G.addNode(5)
-G.addconnection(1,4)
-G.addconnection(4,5)
-G.addconnection(5,2)
-G.addconnection(2,3)
-G.addconnection(5,3)
-t23 = G.getNeibyId(2)
-print(t23)
-
-start = 3
-end = 1
-
-# xrr = breadth_first_search(start=start, end=end, neighbor_function=four_neighbor_function)
-# print(xrr)
-xrr = breadth_first_search(start=start, end=end, neighbor_function=G.getNeibyId)
-print(xrr)
+if __name__ == "__main__":
+    main()
--- home_works/hw1/q3.py
+++ home_works/hw1/q3.py
@@ -1,8 +1,18 @@
-import re
-import time
-from traceback import print_tb
+"""
+ Author: Shlomo Glick
+ Since : 2022-10
+"""
 
 def print_sorted(struct):
+    """
+    The function receives a data structure containing various types and additional structures
+    The function should print the structures sorted at all depth levels
+
+    >>> print_sorted(struct=get_struct1())
+    [(['222', {'a': "{'4', '5', '6'}", 'b': ['Aaa', 'ggg', 't'], 'c': '3'}], 'ff'), '111', 'bbba', {'aa': '2', 'b': '0', 'c': '1', 'xx4': [('2', '5', 's', 'z', {'55': '10', '66': '3', '77': '9'}), '1', '2', '55']}]
+    >>> print_sorted(struct=get_struct2())
+    ['111', [('1', '3', '4'), 'd', {}], 'ta', {'aa': '2', 'b': '0', 'c': '1', 'xx4': [('2', '5', 's', 'z', {'55': '10', '66': '3', '77': '9'}), '1', '2', '55']}, {0: '2'}, {}]
+    """
     print(sorted_struct(struct=struct))
 
 def sorted_struct(struct):
@@ -31,9 +41,8 @@ def sort_tuple(struct):
     return tuple(sort_list(struct))
 
 
-
-
-struct=[
+def get_struct1():
+    return [
     ('ff', 
         [
             '222',
@@ -68,87 +77,40 @@ struct=[
             ]
         }
     ]
-print_sorted(struct=struct)
-struct=[
-    "bbba",
-    "111",
-    {0: 2},
-    {
-        "aa": 2,
-        "c": 1,
-        "b": 0,
-        "xx4": [
-                1,
-                2,
-                "55",
-                (
-                    's',
-                    5,
-                    {
-                        "77": 9,
-                        "55":10,
-                        "66": 3
-                    }, 
-                    '2',
-                    "z"
-                )
-            ]
-        }
-    ]
-print_sorted(struct=struct)
 
-# print_sorted(struct=struct)
-# z65 = ['1', '2', '55']
-# tt65 =('2', '5', 's', 'z', "{'55': '10', '66': '3', '77': '9'}")
-# z65.append(tt65)
-# print(z65)
-# z65 =[]
-# tt1 = 'bbba'
-# tt2 = 111
-# tt3 = 1
-# tt4 = 2
-# tt5 = 55
-# tt6 = ('2', '5', 's', 'z', "{'55': '10', '66': '3', '77': '9'}")
-# z65.append(tt1)
-# z65.append(tt2)
-# z65.append(tt3)
-# z65.append(tt4)
-# z65.append(tt5)
-# z65.append(tt6)
-# print(z65)
-# s = 'a'
-# if hasattr(s, '__iter__'):
-#     print('sy')
-# else:
-#     print('sn')
-# print_sorted(struct={"zz": 4 ,"aa": 2, "c": 1})
+def get_struct2():
+    return[
+        {},
+        ["d",(1,4,3),{}],
+        "ta",
+        "111",
+        {0: 2},
+        {
+            "aa": 2,
+            "c": 1,
+            "b": 0,
+            "xx4": [
+                    1,
+                    2,
+                    "55",
+                    (
+                        's',
+                        5,
+                        {
+                            "77": 9,
+                            "55":10,
+                            "66": 3
+                        }, 
+                        '2',
+                        "z"
+                    )
+                ]
+            }
+        ]
 
-# print_sorted(struct=["bbba", "111"])  #, {"aa": 2, "c": 1}])
+def main():
+    import doctest
+    print(doctest.testmod())
 
-
-# def foo(str2):
-#     if len(str(str2)) < 2:
-#         return str2 
-#     time.sleep(0.2)
-#     try:
-#         str2.sort()
-#         print(f'str2 = {str2}, try')
-#         return str(str2)
-#     except:
-#         print(f'str2 = {str2}, exp')
-#         ans = ''
-#         for d in str2:
-#             x = foo(d)
-#             ans +=x
-#         return ans
-
-
-
-
-
-
-
-
-
-# xx = (1,4,6,3,7)
-# print(sorted(xx))
+if __name__ == "__main__":
+    main()
